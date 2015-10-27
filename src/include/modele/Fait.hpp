b6f6408e000677c02e8c8d99890f353d7fe350bd
Julien Pivard <pivardjulien@gmail.com>
2015-10-27 15:49:09 +0000

    Ajout de la configuration de la documentation. Pour générer la doc se placer à la racine du projet. (la ou se trouve le fichier Doxyfile. taper la commande doxygen. La documentation à été générée dans le dossier doc. enjoy :)
    Ajout de documentation pour la classe Fait.

--- src/include/modele/Fait.hpp
+++ src/include/modele/Fait.hpp
@@ -13,29 +13,49 @@
 #include <string>
 
 /**
- * @namespace
+ * @namespace sysexp
  * */
 namespace sysexp
 {
     /**
-    * @namespace
+    * @namespace modele
     * */
     namespace modele
     {
         /**
          * @class Fait Fait.hpp
-         * La classe abstraite des faits.
+         *
+         * Déclaration de la classe abstraite Fait.
          * */
         class Fait
         {
 
             public:
+                /**
+                 * Constructeur logique
+                 *
+                 * @param[in] nom - Le @ref nom_ du fait.
+                 * */
                 Fait( const std::string & nom );
+                Fait( const Fait & autre ) = delete;
                 Fait & operator= ( const Fait & autre ) = delete;
+
+                /**
+                 * Accesseur
+                 *
+                 * @return Le @ref nom_ du fait.
+                 * */
                 const std::string & lireNom() const;
+
+                /**
+                 * La méthode virtuel destructeur.
+                 * */
                 virtual ~Fait() = default;
 
             protected:
+                /**
+                 * Le nom qu'on a donné au fait.
+                 * */
                 const std::string nom_;
 
         };
