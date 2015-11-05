108287ad51e8648f0a06ed66a06b480b7c0692e7
links <links@binome.com>
2015-11-05 22:09:28 +0000

    le commit a chier, tous les jetons marchent

---
+++ src/tests.cpp
@@ -0,0 +1,54 @@
+#include "jeton.hpp"
+#include "fabriqueJeton.hpp"
+#include "lexical.hpp"
+#include "gtest/gtest.h"
+#include <sstream>
+#include <iostream>
+using namespace sysexp::builders;
+#include "jeton.hpp"
+
+#include "fabriqueJeton.hpp"
+
+#include "lexical.hpp"
+
+#include <sstream>
+
+#include <istream>
+
+#include <fstream>
+
+#include <iostream>
+
+using namespace sysexp::builders;
+
+void
+representationJeton(std::istream & chaine) {
+        const char* types[] = {"parentheseOuvrante", "parentheseFermante", "superieur", "inferieur",
+                              "operateurPlus", "operateurMoins", "operateurMul", "operateurDiv",
+                              "egal", "finExpression", "identificateur", "entier",
+                              "si", "non", "et", "alors", "separateur", "inconnu", "finFichier"};
+        Lexical lexical = Lexical(chaine);
+        Jeton jeton = lexical.suivant();
+        while(!jeton.estFinFichier()){
+                sysexp::type::Type typeJeton = jeton.lireType();
+                std::cout << types[typeJeton] << "(" << jeton.lireRepresentation() << ")" << std::endl;
+                jeton = lexical.suivant();
+        }
+}
+
+int
+main(int argc, char* argv[] ){
+    if( argc > 1 ){
+        std::cout << "Usage "
+            << argv[0]
+            << std::endl;
+    }
+    if( argc != 1 ){
+        std::cerr << "Trop d'argument, tartine de pus !"
+            << std::endl;
+        return EXIT_FAILURE;
+    }
+        std::istringstream chaine("si(pape et)alors bite, non  +  / = *  < > - 4; ?!_");
+        representationJeton(chaine);
+        return EXIT_SUCCESS;
+}
