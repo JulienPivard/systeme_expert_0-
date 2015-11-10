9fa2b89a4ed8ad011a9f2f69013f05536d5b106c
links <links@binome.com>
2015-11-10 00:41:48 +0000

    doc et uml de syntaxique fait ( doc et uml temporaires car les methodes retourneront des objets)

--- src/include/builders/syntaxique.hpp
+++ src/include/builders/syntaxique.hpp
@@ -37,50 +37,138 @@
 				*/
 				const Lexical lireLexical();
 
+				/**
+				 * parseur de la grammaire lorraine.
+				 *
+				*/
 				void parser();
-				
+
 			protected:
 
+				/**
+				 * brique parsant les déclarations.
+				 *
+				*/
 				void declarations();
 
+				/**
+				 * brique parsant les déclarations booléennes.
+				 *
+				*/
 				void declarations_bool();
 
+				/**
+				 * brique parsant les déclarations symboliques.
+				 *
+				*/
 				void declarations_symb();
 
+				/**
+				 * brique parsant les déclarations entieres.
+				 *
+				*/
 				void declarations_ent();
 
+				/**
+				 * brique parsant les faits.
+				 *
+				*/
 				void listeFaits(std::string valeur);
 				
+				/**
+				 * brique parsant les regles.
+				 *
+				*/
 				void regles();
 
+				/**
+				 * brique parsant une regle.
+				 *
+				*/
 				void regle();
 
+				/**
+				 * brique parsant une regle sans premisse.
+				 *
+				*/
 				void regle_sans_premisse();
 
+				/**
+				 * brique parsant une conclusion.
+				 *
+				*/
 				void conclusion();
 
+				/**
+				 * brique parsant une conclusion booléenne.
+				 *
+				*/
 				void conclusion_booleenne();
 
+				/**
+				 * brique parsant une conclusion symbolique.
+				 *
+				*/
 				void conclusion_symbolique();
 
+				/**
+				 * brique parsant une conclusion entiere.
+				 *
+				*/
 				void conclusion_entiere();
 
+				/**
+				 * brique parsant une expression entiere.
+				 *
+				*/
 				void expressionEntiere();
 
+				/**
+				 * brique parsant un terme.
+				 *
+				*/
 				void terme();
 
+				/**
+				 * brique parsant un facteur.
+				 *
+				*/
 				void facteur();
 
+				/**
+				 * brique parsant une regle avec premisse.
+				 *
+				*/
 				void regle_avec_premisse();
 
+				/**
+				 * brique parsant une condition.
+				 *
+				*/
 				void condition();
-
+				
+				/**
+				 * brique parsant une premisse.
+				 *
+				*/
 				void premisse();
 
+				/**
+				 * brique parsant une premisse booléenne.
+				 *
+				*/
 				void premisse_booleenne();
 
+				/**
+				 * brique parsant une premisse symbolique.
+				 *
+				*/
 				void premisse_symbolique();
 
+				/**
+				 * brique parsant une premisse entiere.
+				 *
+				*/
 				void premisse_entiere();
 				
 			protected:
@@ -93,6 +181,9 @@
 				*/
 				Jeton precharge_;
 
+				/**
+				 * map de faits (temporaire).
+				*/
 				std::map <std::string, std::string> faits_;
 
 
